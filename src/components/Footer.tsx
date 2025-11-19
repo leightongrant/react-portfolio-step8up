db193ae656362a4ac8a83c0eec6f7de3254d0572
Leighton Grant <oneal.grant@outlook.com>
2025-11-19 10:09:37 +0000

    feat: add Layout, Header, and Footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+export const Footer = () => {
+	return (
+		<Container>
+			<Row>
+				<Col>Footer</Col>
+			</Row>
+		</Container>
+	)
+}
---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react'
+import { Header } from './Header'
+import { Footer } from './Footer'
+
+type MyComponentProps = {
+	children: ReactNode
+}
+
+export const Layout = ({ children }: MyComponentProps) => {
+	return (
+		<>
+			<Header />
+			<>{children}</>
+			<Footer />
+		</>
+	)
+}
