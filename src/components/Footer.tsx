af3c1f0aca8d2a19a2a0a0eb98a8117142011197
Leighton Grant <oneal.grant@outlook.com>
2025-11-19 13:22:45 +0000

    refactor(footer): Improve footer structure and styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,22 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Stack from 'react-bootstrap/Stack'
 
 export const Footer = () => {
 	return (
-		<Container>
-			<Row>
-				<Col>Footer</Col>
-			</Row>
-		</Container>
+		<Stack className='site-footer-container bg-dark text-light'>
+			<Container>
+				<Row>
+					<Col>&copy; Copyright 2025 | Leighton Grant</Col>
+					<Col>
+						<Row>
+							<Col>LinkedIn</Col>
+							<Col>GitHub</Col>
+						</Row>
+					</Col>
+				</Row>
+			</Container>
+		</Stack>
 	)
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export const Layout = ({ children }: MyComponentProps) => {
 	return (
 		<>
 			<Header />
-			<>{children}</>
+			{children}
 			<Footer />
 		</>
 	)
