f1906689ff0d0f92fef24df04c425f2edb3bdecf
Leighton Grant <oneal.grant@outlook.com>
2025-11-21 11:54:29 +0000

    feat(app): add new pages and update routing feat(deps): add react-icons and react-markdown style(css): update body color and add padding utilities fix(images): update carousel images

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import { Layout } from './components/Layout'
 import { Routes, Route } from 'react-router-dom'
 import { Homepage } from './pages/Homepage'
 import { ProjectsPage } from './pages/Projects'
+import { AboutPage } from './pages/About'
+import { ContactPage } from './pages/Contact'
+import { PageBanner } from './components/PageBanner'
+import { useLocation } from 'react-router-dom'
+import { ProjectDetails } from './components/ProjectDetails'
 
 function App() {
+	const { pathname } = useLocation()
+
 	return (
 		<Layout>
+			{pathname !== '/' && <PageBanner />}
 			<Routes>
 				<Route
 					index
@@ -15,6 +23,18 @@ function App() {
 				<Route
 					path='/projects'
 					element={<ProjectsPage />}
+				/>
+				<Route
+					path='/projects/:id'
+					element={<ProjectDetails />}
+				/>
+				<Route
+					path='/about'
+					element={<AboutPage />}
+				/>
+				<Route
+					path='/contact'
+					element={<ContactPage />}
 				/>
 			</Routes>
 		</Layout>
