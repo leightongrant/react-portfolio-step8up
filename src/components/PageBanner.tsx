3e0063ce83b606c4c11c5d5f426ba4d174bb6873
Leighton Grant <oneal.grant@outlook.com>
2025-11-21 11:53:19 +0000

    feat(page-banner): Add PageBanner component with styling

---
+++ src/components/PageBanner.tsx
@@ -0,0 +1,18 @@
+import Stack from 'react-bootstrap/Stack'
+import Container from 'react-bootstrap/Container'
+import './PageBanner.css'
+import { useLocation } from 'react-router-dom'
+export const PageBanner = () => {
+	const { pathname } = useLocation()
+	return (
+		<Stack className='page-banner-container bg-secondary'>
+			<Stack className='banner-overlay'>
+				<Container className='flex-grow-1'>
+					<Stack className='page-banner-content justify-content-center h-100'>
+						<span className='fs-1 fw-bold'>{pathname.slice(1).toLocaleUpperCase()}</span>
+					</Stack>
+				</Container>
+			</Stack>
+		</Stack>
+	)
+}
