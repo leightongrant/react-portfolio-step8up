a3ddf9d30dfd31638f5986f600be631c567d4022
Leighton Grant <oneal.grant@outlook.com>
2025-11-21 11:52:58 +0000

    feat(layout): Add CSS styling for layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react'
 import { Header } from './Header'
 import { Footer } from './Footer'
+import './Layout.css'
 
 type MyComponentProps = {
 	children: ReactNode
@@ -8,10 +9,10 @@ type MyComponentProps = {
 
 export const Layout = ({ children }: MyComponentProps) => {
 	return (
-		<>
+		<div className='page-wrapper'>
 			<Header />
 			{children}
 			<Footer />
-		</>
+		</div>
 	)
 }
