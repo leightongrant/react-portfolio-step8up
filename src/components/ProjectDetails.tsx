06369584ed9cc7bf8c29e4683301e85a8412aac4
Leighton Grant <oneal.grant@outlook.com>
2025-11-23 21:27:33 +0000

    refactor(ProjectDetails): simplify data fetching with useFetch hook

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -1,5 +1,4 @@
 import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 import { PreLoader } from '../preloader/preloaders'
 import { Error } from '../error/errors'
 import ReactMarkdown from 'react-markdown'
@@ -7,39 +6,20 @@ import Container from 'react-bootstrap/Container'
 import Stack from 'react-bootstrap/Stack'
 import { Link } from 'react-router-dom'
 import { FaArrowLeftLong } from 'react-icons/fa6'
+import { useFetch } from '../hooks/hooks'
 
 export const ProjectDetails = () => {
-	const [readme, setReadme] = useState<any>(null)
-	const [error, setError] = useState<any>(null)
 	const params = useParams()
 	const url = `https://api.github.com/repos/leightongrant/${params.id}/readme`
-
-	const getRepo = async () => {
-		try {
-			const response = await fetch(url, {
-				headers: {
-					Accept: 'application/vnd.github.v3.raw',
-				},
-			})
-			if (response.ok) {
-				const readme = await response.text()
-				setReadme(readme)
-				return
-			}
-			const error = await response.json()
-			console.log(error)
-			setError(error)
-		} catch (error) {
-			console.log(error)
-			setError(error)
-		}
+	const fetchOptions = {
+		headers: {
+			Accept: 'application/vnd.github.v3.raw',
+		},
 	}
-	useEffect(() => {
-		getRepo()
-	}, [])
+	const { loading, data, error } = useFetch(url, fetchOptions)
 
 	if (error) return <Error error={error} />
-	if (!readme) return <PreLoader />
+	if (loading) return <PreLoader />
 	return (
 		<Stack
 			className='block-padding-large'
@@ -57,8 +37,9 @@ export const ProjectDetails = () => {
 						),
 					}}
 				>
-					{readme}
+					{typeof data === 'string' ? data : ''}
 				</ReactMarkdown>
+
 				<Stack>
 					<Link
 						to='/projects'
